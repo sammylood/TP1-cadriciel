41d11a367ca2ac6a5c135315149d787a28b33d3f
Sammylood <sammy.romeo@hotmail.com>
2025-03-08 22:31:34 -0500

    ajout Models Etudiant et Ville avec commande php artisan make:model 'Model Name' -m

---
+++ Maisonneuve0366635/database/migrations/2025_03_04_102942_create_etudiants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description')->nullable(); // peut être vide
+            $table->boolean('completed')->default(0);
+            $table->date('due_date')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
